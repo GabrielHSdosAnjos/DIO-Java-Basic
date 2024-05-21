c61dd8de55eb00d944c57d48a5b39786036238c3
Gabriel <gabrielhonorioa@gmail.com>
2024-05-21 16:38:17 -0300

    teste1

--- Meu primeiro Projeto Java/src/App.java
+++ Meu primeiro Projeto Java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("OLá a todos, neymar jr is best player of this generation");
+        System.out.println("OLá a todos, meu nome é gabriel");
     }
 }
