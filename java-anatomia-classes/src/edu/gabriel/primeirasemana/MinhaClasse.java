3e62303d5ac885018e92ab08ebde537ff979c531
Gabriel <gabrielhonorioa@gmail.com>
2024-06-04 19:09:21 -0300

    top

--- java-anatomia-classes/src/edu/gabriel/primeirasemana/MinhaClasse.java
+++ java-anatomia-classes/src/edu/gabriel/primeirasemana/MinhaClasse.java
@@ -7,9 +7,24 @@ public static void main(String[] args) {
     int anoFabricacao = 2024;
     boolean verdadeira = False 
     anoFabricacao = 2016
+    //não recomendado
+    double salMedio = 1500.23  //variável abreviada, o que dificulta a compreensão
+    String emails = "aluno@escola.com" //confuso se a variável seria um array ou único e-mail
+    String myName = "JOSEPH" //se idioma pt-BR, o valor poder ser de outro idioma mas o nome da variável não 
+    //recomendado
+    double salarioMedio=1500.23;
+    String email ="aluno@escola.com";
+    String [] emails = {"aluno@escola.com","professor@escola.com"}
+    String meuNome = "JOSEPH" 
+    somar(int n1, int n2){}
 
+    abrirConexao(){}
 
+    concluirProcessamento() {}
 
+    findById(int id){} // não se assuste, você verá muito método em inglês em sua jornada
+
+    calcularImprimir(){} // há algo de errado neste método, ele deveria ter uma única finalidade
 }
 public static String nomeCompleto (String primeiroNome,String segundonome ){
     return primeiroNome
