f0eb13647cc46f3800fe428d549de17d644a1189
Gabriel <gabrielhonorioa@gmail.com>
2024-06-10 19:02:05 -0300

    operadores2

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //classe Operadores.java
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+        }
+}
