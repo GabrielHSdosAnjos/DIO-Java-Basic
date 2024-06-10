66d2c1ae4f687bb5f241de5d9a4c9e21c2e42162
Gabriel <gabrielhonorioa@gmail.com>
2024-06-10 19:46:32 -0300

    topdms

--- Operadores/src/Operadores.java
+++ Operadores/src/Operadores.java
@@ -1,6 +1,7 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nomecompleto="Linguagem " + "Java";
+        System.out.println(nomecompleto);
         //classe Operadores.java
         String nome = "GLEYSON";
         int idade = 22;
@@ -8,5 +9,11 @@ public class Operadores {
         char sexo = 'M';
         boolean doadorOrgao = false;
         Date dataNascimento = new Date();
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
         }
 }
