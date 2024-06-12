6562abc9fb21f790269579a922f8ea40ae74d868
Gabriel <gabrielhonorioa@gmail.com>
2024-06-12 16:39:35 -0300

    smarttv

--- Operadores/src/Operadores.java
+++ Operadores/src/Operadores.java
@@ -2,18 +2,5 @@ public class Operadores {
     public static void main(String[] args) throws Exception {
         String nomecompleto="Linguagem " + "Java";
         System.out.println(nomecompleto);
-        //classe Operadores.java
-        String nome = "GLEYSON";
-        int idade = 22;
-        double peso = 68.5;
-        char sexo = 'M';
-        boolean doadorOrgao = false;
-        Date dataNascimento = new Date();
-        double soma = 10.5 + 15.7;
-        int subtração = 113 - 25;
-        int multiplicacao = 20 * 7;
-        int divisao = 15 / 3;
-        int modulo = 18 % 3;
-        double resultado = (10 * 7) + (20/4);
         }
 }
