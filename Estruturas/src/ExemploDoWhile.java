91e80acfbf36376e2a5f6bded011d4895b207cb1
Gabriel <gabrielhonorioa@gmail.com>
2024-07-04 15:38:20 -0300

    tudo certo

--- Estruturas/src/ExemploDoWhile.java
+++ Estruturas/src/ExemploDoWhile.java
@@ -2,8 +2,7 @@
 
 import java.util.Random;
 public class ExemploDoWhile {
-public static void main(String[] args) {
-	public static void main(String[] args) {
+public static void main(String[] args) {{
 		System.out.println("Discando...");
 		
 		do {
@@ -12,8 +11,8 @@ public static void main(String[] args) {
 		
 		}while(tocando());
 		
-		System.out.println("Alô !!!");
-	}
+		System.out.println("Alô !!!");}
+}
 	private static boolean tocando() {
 		boolean atendeu = new Random().nextInt(3)==1;
 		System.out.println("Atendeu? " + atendeu);
